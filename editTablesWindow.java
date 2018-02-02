1544765adf73ea91d452d99b651a1e154aa6c915
JoeMuff999 <36085760+JoeMuff999@users.noreply.github.com>
2018-02-02 13:33:16 -0600

    Add files via upload

---
+++ editTablesWindow.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.io.Serializable;
+public class editTablesWindow extends Frame
+{
+    public editTablesWindow()
+    {
+        JFrame edit = new JFrame();
+        
+
+        
+        
+        edit.setSize(400,400);
+        edit.setVisible(true);
+    }
+}
